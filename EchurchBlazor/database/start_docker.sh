e914b0e4ae70a2965de843e5fa403e3600d1cf7a
Gong Wang <gong@coreschedule.com>
2021-03-04 22:55:56 +1300

    add docker starter

--- EchurchBlazor/database/start_docker.sh
+++ EchurchBlazor/database/start_docker.sh
@@ -1,8 +1,13 @@
-EXISTING_CONTAINTER=$(docker ps -a | grep "mssql-server"| lc -l)
+EXISTING_CONTAINER=$(docker ps -a | grep "mssql-server"| wc -l | sed -e 's/^[[:space:]]*//')
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-echo $EXISTING_CONTAINER
+if [ "$EXISTING_CONTAINER" -ge "1" ]; then
 
-exit
+    echo "Removing existing container..."
+    docker stop mssql-server
+    docker rm mssql-server
 
+fi
 
-docker run -name mssql-server -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=mssqlMaster@local' -e 'MSSQL_PID=Express' -p 127.0.0.1:1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+echo "Starting new container..."
+docker run --name mssql-server -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=mssqlMaster@local' -e 'MSSQL_PID=Express' -v $SCRIPT_PATH/data:/var/opt/mssql/data -p 127.0.0.1:1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
